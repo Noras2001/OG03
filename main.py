7f82cd4232c5a12051267c782f3cf7508c5fcbce
Noras2001 <nora.serdyukova@gmail.com>
2024-12-11 19:18:25 -0300

    main consts

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import pygame
+import random
 
 pygame.init()
+
+# constants part
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("img/tir_icon.png")
+pygame.display.set_icon(icon)
+
+target = pygame.image.load("img/target.png")
+target_width = 118
+target_height = 118
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+# Скорость движения таргета по осям x и y
+speed_x = random.randint(-5, 5)
+speed_y = random.randint(-5, 5)
+
+
+
+
 
 running = True
 while running:
